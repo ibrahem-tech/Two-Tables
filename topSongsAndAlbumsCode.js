0cb61eb9778815b712c0b9adb2e5c1070ecc9231
Ibrahem Rian <ryibrahem1210@gmail.com>
2020-04-09 20:56:05 -0400

    multi-search-and-range-search

--- topSongsAndAlbumsCode.js
+++ topSongsAndAlbumsCode.js
@@ -71,3 +71,59 @@ function artistSearch() {
       });
 
 }
+
+function multiSearch() {
+    var query = "SELECT artist FROM top5000 GROUP BY artist HAVING count(*) > 1";
+    connection.query(query, function(err, res) {
+      for (var i = 0; i < res.length; i++) {
+        console.log(res[i].artist);
+      }
+      runSearch();
+    });
+  }
+
+  function rangeSearch() {
+    inquirer
+      .prompt([
+        {
+          name: "start",
+          type: "input",
+          message: "Enter starting position: ",
+          validate: function(value) {
+            if (isNaN(value) === false) {
+              return true;
+            }
+            return false;
+          }
+        },
+        {
+          name: "end",
+          type: "input",
+          message: "Enter ending position: ",
+          validate: function(value) {
+            if (isNaN(value) === false) {
+              return true;
+            }
+            return false;
+          }
+        }
+      ])
+      .then(function(answer) {
+        var query = "SELECT position,song,artist,year FROM top5000 WHERE position BETWEEN ? AND ?";
+        connection.query(query, [answer.start, answer.end], function(err, res) {
+          for (var i = 0; i < res.length; i++) {
+            console.log(
+              "Position: " +
+                res[i].position +
+                " || Song: " +
+                res[i].song +
+                " || Artist: " +
+                res[i].artist +
+                " || Year: " +
+                res[i].year
+            );
+          }
+          runSearch();
+        });
+      });
+  }
