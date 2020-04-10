1a43813399e6ec30c8e3c33026f47c349b890fbd
Ibrahem Rian <ryibrahem1210@gmail.com>
2020-04-09 20:47:22 -0400

    runsearch

--- topSongsAndAlbumsCode.js
+++ topSongsAndAlbumsCode.js
@@ -12,3 +12,44 @@ var connection = mysql.createConnection({
 });
 
 
+connection.connect(function(err){
+    if (err) throw err;
+    runSearch()
+});
+
+function runSearch(){
+    inquirer.prompt({
+        name: "action",
+        type: "rawlist",
+        message: "what yould you like to do now?",
+        choises: [
+            "Find songs by artist",
+        "Find all artists who appear more than once",
+        "Find data within a specific range",
+        "Search for a specific song",
+        "Find artists with a top song and top album in the same year"
+         ]
+    })
+    .then(function(answer){
+        switch (answer.action){
+            case "Find songs by artist": artistSearch();
+            break;
+            case "Find all artists who appear more than once":
+        multiSearch();
+        break;
+
+      case "Find data within a specific range":
+        rangeSearch();
+        break;
+
+      case "Search for a specific song":
+        songSearch();
+        break;
+
+      case "Find artists with a top song and top album in the same year":
+        songAndAlbumSearch();
+        break;
+        }
+    });
+}
+
