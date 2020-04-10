8782c2c68bfdde9e9561f7be7c755cdd20c2dbc2
Ibrahem Rian <ryibrahem1210@gmail.com>
2020-04-09 20:54:02 -0400

    artist-search

--- topSongsAndAlbumsCode.js
+++ topSongsAndAlbumsCode.js
@@ -53,3 +53,21 @@ function runSearch(){
     });
 }
 
+function artistSearch() {
+    inquirer
+      .prompt({
+        name: "artist",
+        type: "input",
+        message: "What artist would you like to search for?"
+      })
+      .then(function(answer) {
+        var query = "SELECT position, song, year FROM top5000 WHERE ?";
+        connection.query(query, { artist: answer.artist }, function(err, res) {
+          for (var i = 0; i < res.length; i++) {
+            console.log("Position: " + res[i].position + " || Song: " + res[i].song + " || Year: " + res[i].year);
+          }
+          runSearch();
+        });
+      });
+
+}
